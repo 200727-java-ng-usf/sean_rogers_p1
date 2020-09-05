443759c30ee1c38ac893661d3c051263a1ee49ab
SeanRog <seanmikerog@gmail.com>
2020-09-04 22:06:21 -0700

    test .gitignore

--- Project1/src/main/java/com/revature/utilities/DAOUtilities.java
+++ Project1/src/main/java/com/revature/utilities/DAOUtilities.java
@@ -2,10 +2,7 @@ package com.revature.utilities;
 
 import javax.servlet.Servlet;
 import javax.servlet.ServletContext;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -42,17 +39,23 @@ public class DAOUtilities {
 
         try {
             Class.forName("org.postgresql.Driver");
-            //props.load(new FileReader("application.properties"));
-            System.out.println(props.toString());
+            props.load(new FileReader("C:\\Users\\Mr-Ro\\repos\\reva-repos\\batch-repos\\200727-jang-ng-usf\\sean_rogers_p1\\Project1\\src\\main\\resources\\application.properties"));
+            //System.out.println("Line44: " + new File("C:\\Users\\Mr-Ro\\repos\\reva-repos\\batch-repos\\200727-jang-ng-usf\\sean_rogers_p1\\Project1\\src\\main\\resources\\application.properties").getPath());
 
-            connection = DriverManager.getConnection(props.getProperty("url"),
+            /*connection = DriverManager.getConnection("jdbc:postgresql://java-ng-usf-200727.c00sdxc1dzhk.us-east-2.rds.amazonaws.com:5432/postgres?currentSchema=project1",
+                    "project1_user",
+                    "superman");*/
+
+            connection = DriverManager.getConnection(
+                    props.getProperty("url"),
                     props.getProperty("username"),
-                    props.getProperty("password"));
+                    props.getProperty("password")
+            );
         } catch (ClassNotFoundException | SQLException e) {
             e.printStackTrace();
-        } /*catch (IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
-        }*/
+        }
 
         if (connection == null) {
             try {
---
+++ Project1/src/main/java/com/revature/utilities/DBProperties.java
@@ -0,0 +1,7 @@
+package com.revature.utilities;
+
+public class DBProperties {
+
+    //this should be ignored
+
+}
