e45bb4012f4cb058cee54ec7638abe3a3e63fa3a
SeanRog <seanmikerog@gmail.com>
2020-09-12 12:52:55 -0700

    DeleteUserServletTest and UpdateUserServletTest completed. Admin servlet tests complete

--- Project1/src/test/java/com/revature/servlets/admin/DeleteUserServletTest.java
+++ Project1/src/test/java/com/revature/servlets/admin/DeleteUserServletTest.java
@@ -1,6 +1,7 @@
 package com.revature.servlets.admin;
 
 import com.revature.dao.ErsUsersDAO;
+import com.revature.exceptions.NotAuthorizedException;
 import com.revature.exceptions.UsernameNotFoundException;
 import com.revature.model.ErsUser;
 import org.junit.After;
@@ -12,6 +13,7 @@ import org.mockito.Mockito;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 public class DeleteUserServletTest {
@@ -34,6 +36,7 @@ public class DeleteUserServletTest {
     public void deleteUserThatDoesntExist() throws ServletException, IOException {
         HttpServletRequest mockRequest = Mockito.mock(HttpServletRequest.class);
         HttpServletResponse mockResponse = Mockito.mock(HttpServletResponse.class);
+        HttpSession session = Mockito.mock(HttpSession.class);
 
         Mockito.when(mockRequest.getParameter("username")).thenReturn("seanrog-1");
         Mockito.when(mockRequest.getParameter("password")).thenReturn("seanrog1password");
@@ -41,6 +44,18 @@ public class DeleteUserServletTest {
         Mockito.when(mockRequest.getParameter("lastname")).thenReturn("rogers1");
         Mockito.when(mockRequest.getParameter("email")).thenReturn("seanrog1@gmail.com");
         Mockito.when(mockRequest.getParameter("role")).thenReturn("1");
+
+        ErsUser adminUser = new ErsUser();
+        adminUser.setId(1);
+        adminUser.setUsername("seanrog1");
+        adminUser.setPassword("seanrog1password");
+        adminUser.setFirstName("sean1");
+        adminUser.setLastName("rogers1");
+        adminUser.setEmail("seanmikerog1@gmail.com");
+        adminUser.setUserRoleId(1);
+
+        Mockito.when(session.getAttribute("user")).thenReturn(adminUser);
+        Mockito.when(mockRequest.getSession()).thenReturn(session);
 
         Mockito.when(mockErsUsersDAO.getUserByUsername("seanrog-1")).thenReturn(null);
 
@@ -52,6 +67,7 @@ public class DeleteUserServletTest {
     public void deleteExistingUser() throws ServletException, IOException {
         HttpServletRequest mockRequest = Mockito.mock(HttpServletRequest.class);
         HttpServletResponse mockResponse = Mockito.mock(HttpServletResponse.class);
+        HttpSession session = Mockito.mock(HttpSession.class);
 
         Mockito.when(mockRequest.getParameter("username")).thenReturn("seanrog1");
         Mockito.when(mockRequest.getParameter("password")).thenReturn("seanrog1password");
@@ -60,17 +76,70 @@ public class DeleteUserServletTest {
         Mockito.when(mockRequest.getParameter("email")).thenReturn("seanrog1@gmail.com");
         Mockito.when(mockRequest.getParameter("role")).thenReturn("1");
 
-        ErsUser user = new ErsUser();
+        ErsUser adminUser = new ErsUser();
+        adminUser.setId(1);
+        adminUser.setUsername("seanrog1");
+        adminUser.setPassword("seanrog1password");
+        adminUser.setFirstName("sean1");
+        adminUser.setLastName("rogers1");
+        adminUser.setEmail("seanmikerog1@gmail.com");
+        adminUser.setUserRoleId(1);
 
-        user.setId(1);
-        user.setUsername("seanrog1");
-        user.setPassword("seanrog1password");
-        user.setFirstName("sean1");
-        user.setLastName("rog1");
-        user.setEmail("seanmikerog1@gmail.com");
-        user.setUserRoleId(1);
+        Mockito.when(session.getAttribute("user")).thenReturn(adminUser);
+        Mockito.when(mockRequest.getSession()).thenReturn(session);
 
-        Mockito.when(mockErsUsersDAO.getUserByUsername("seanrog1")).thenReturn(user);
+        ErsUser targetUser = new ErsUser();
+
+        targetUser.setId(1);
+        targetUser.setUsername("seanrog1");
+        targetUser.setPassword("seanrog1password");
+        targetUser.setFirstName("sean1");
+        targetUser.setLastName("rog1");
+        targetUser.setEmail("seanmikerog1@gmail.com");
+        targetUser.setUserRoleId(1);
+
+        Mockito.when(mockErsUsersDAO.getUserByUsername("seanrog1")).thenReturn(targetUser);
+
+        sut.doPost(mockRequest, mockResponse);
+
+    }
+
+    @Test (expected = NotAuthorizedException.class)
+    public void deleteExistingUserAsAnUnauthorizedUser() throws ServletException, IOException {
+        HttpServletRequest mockRequest = Mockito.mock(HttpServletRequest.class);
+        HttpServletResponse mockResponse = Mockito.mock(HttpServletResponse.class);
+        HttpSession session = Mockito.mock(HttpSession.class);
+
+        Mockito.when(mockRequest.getParameter("username")).thenReturn("seanrog1");
+        Mockito.when(mockRequest.getParameter("password")).thenReturn("seanrog1password");
+        Mockito.when(mockRequest.getParameter("firstname")).thenReturn("sean1");
+        Mockito.when(mockRequest.getParameter("lastname")).thenReturn("rogers1");
+        Mockito.when(mockRequest.getParameter("email")).thenReturn("seanrog1@gmail.com");
+        Mockito.when(mockRequest.getParameter("role")).thenReturn("1");
+
+        ErsUser adminUser = new ErsUser();
+        adminUser.setId(3);
+        adminUser.setUsername("seanrog3");
+        adminUser.setPassword("seanrog3password");
+        adminUser.setFirstName("sean3");
+        adminUser.setLastName("rogers3");
+        adminUser.setEmail("seanmikerog3@gmail.com");
+        adminUser.setUserRoleId(2);
+
+        Mockito.when(session.getAttribute("user")).thenReturn(adminUser);
+        Mockito.when(mockRequest.getSession()).thenReturn(session);
+
+        ErsUser targetUser = new ErsUser();
+
+        targetUser.setId(1);
+        targetUser.setUsername("seanrog1");
+        targetUser.setPassword("seanrog1password");
+        targetUser.setFirstName("sean1");
+        targetUser.setLastName("rog1");
+        targetUser.setEmail("seanmikerog1@gmail.com");
+        targetUser.setUserRoleId(1);
+
+        Mockito.when(mockErsUsersDAO.getUserByUsername("seanrog1")).thenReturn(targetUser);
 
         sut.doPost(mockRequest, mockResponse);
 
--- Project1/src/test/java/com/revature/servlets/admin/UpdateUserServletTest.java
+++ Project1/src/test/java/com/revature/servlets/admin/UpdateUserServletTest.java
@@ -1,8 +1,11 @@
 package com.revature.servlets.admin;
 
 import com.revature.dao.ErsUsersDAO;
+import com.revature.exceptions.NotAuthorizedException;
 import com.revature.exceptions.UsernameNotFoundException;
 import com.revature.model.ErsUser;
+import com.sun.deploy.net.HttpRequest;
+import com.sun.deploy.net.HttpResponse;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -11,6 +14,7 @@ import org.mockito.Mockito;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 public class UpdateUserServletTest {
@@ -34,6 +38,7 @@ public class UpdateUserServletTest {
 
         HttpServletRequest mockRequest = Mockito.mock(HttpServletRequest.class);
         HttpServletResponse mockResponse = Mockito.mock(HttpServletResponse.class);
+        HttpSession session = Mockito.mock(HttpSession.class);
 
         Mockito.when(mockRequest.getParameter("username")).thenReturn("seanrog-1");
         Mockito.when(mockRequest.getParameter("password")).thenReturn("seanrog1password");
@@ -42,6 +47,17 @@ public class UpdateUserServletTest {
         Mockito.when(mockRequest.getParameter("email")).thenReturn("seanmikerog1@gmail.com");
         Mockito.when(mockRequest.getParameter("role")).thenReturn("1");
 
+        ErsUser adminUser = new ErsUser();
+        adminUser.setId(1);
+        adminUser.setUsername("seanrog1");
+        adminUser.setPassword("seanrog1password");
+        adminUser.setFirstName("sean1");
+        adminUser.setLastName("rogers1");
+        adminUser.setEmail("seanmikerog1@gmail.com");
+        adminUser.setUserRoleId(1);
+
+        Mockito.when(mockRequest.getSession()).thenReturn(session);
+        Mockito.when(session.getAttribute("user")).thenReturn(adminUser);
         Mockito.when(mockErsUsersDAO.getUserByUsername("seanrog-1")).thenReturn(null);
 
         sut.doPost(mockRequest,mockResponse);
@@ -54,6 +70,7 @@ public class UpdateUserServletTest {
 
         HttpServletRequest mockRequest = Mockito.mock(HttpServletRequest.class);
         HttpServletResponse mockResponse = Mockito.mock(HttpServletResponse.class);
+        HttpSession session = Mockito.mock(HttpSession.class);
 
         Mockito.when(mockRequest.getParameter("username")).thenReturn("seanrog1");
         Mockito.when(mockRequest.getParameter("password")).thenReturn("seanrog1password");
@@ -61,6 +78,15 @@ public class UpdateUserServletTest {
         Mockito.when(mockRequest.getParameter("lastName")).thenReturn("rogers1");
         Mockito.when(mockRequest.getParameter("email")).thenReturn("seanmikerog1@gmail.com");
         Mockito.when(mockRequest.getParameter("role")).thenReturn("1");
+
+        ErsUser adminUser = new ErsUser();
+        adminUser.setId(1);
+        adminUser.setUsername("seanrog1");
+        adminUser.setPassword("seanrog1password");
+        adminUser.setFirstName("sean1");
+        adminUser.setLastName("rogers1");
+        adminUser.setEmail("seanmikerog1@gmail.com");
+        adminUser.setUserRoleId(1);
 
         ErsUser user = new ErsUser();
         user.setId(1);
@@ -71,12 +97,45 @@ public class UpdateUserServletTest {
         user.setEmail("seanmikerog1@gmail.com");
         user.setUserRoleId(1);
 
-
+        Mockito.when(mockRequest.getSession()).thenReturn(session);
+        Mockito.when(session.getAttribute("user")).thenReturn(adminUser);
         Mockito.when(mockErsUsersDAO.getUserByUsername("seanrog1")).thenReturn(user);
 
         sut.doPost(mockRequest,mockResponse);
 
+    }
+
+    @Test (expected = NotAuthorizedException.class)
+    public void updateExistingUserAsAnUnauthorizedUser() throws ServletException, IOException {
+
+        HttpServletRequest mockRequest = Mockito.mock(HttpServletRequest.class);
+        HttpServletResponse mockResponse = Mockito.mock(HttpServletResponse.class);
+        HttpSession session = Mockito.mock(HttpSession.class);
+
+        ErsUser targetUser = new ErsUser();
+        targetUser.setId(5);
+        targetUser.setUsername("seanrog5");
+        targetUser.setPassword("seanrog5password");
+        targetUser.setFirstName("sean5");
+        targetUser.setLastName("rogers5");
+        targetUser.setEmail("seanmikerog5@gmail.com");
+        targetUser.setUserRoleId(3);
+
+        ErsUser adminUser = new ErsUser();
+        adminUser.setId(3);
+        adminUser.setUsername("seanrog3");
+        adminUser.setPassword("seanrog3password");
+        adminUser.setFirstName("sean3");
+        adminUser.setLastName("rogers3");
+        adminUser.setEmail("seanmikerog3@gmail.com");
+        adminUser.setUserRoleId(2);
+
+        Mockito.when(mockRequest.getSession()).thenReturn(session);
+        Mockito.when(session.getAttribute("user")).thenReturn(adminUser);
+
+        sut.doPost(mockRequest,mockResponse);
 
     }
+
 
 }
